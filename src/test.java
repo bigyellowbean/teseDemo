e5c1c48abba3bad364852c0b628fb185e6a2b328
Cs <shuochen@zjft.com>
2020-04-15 16:53:20 +0800

    第二次提交

--- src/test.java
+++ src/test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
+        System.out.println("第二次提交");
         File file = new File("E:\\plugins");
         File[] files = file.listFiles();
         for (File file1 : files) {
